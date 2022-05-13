cf2ac47c49a218dd098aad136e2a5e538daffb7d
MGundam <93197249+MGundam@users.noreply.github.com>
2022-05-13 11:28:57 +0800

    SCARA_V3_Simulation in Robotics Toolbox
    The library used in this python program is Robotics Toolbox by Peter Corke.
    
    You need to have Robotics Toolbox library by Peter Corke to run the program.

---
+++ SCARA_V3_6DOF.py
@@ -0,0 +1,133 @@
+
+import roboticstoolbox as rtb
+import numpy as np
+from roboticstoolbox import DHRobot, RevoluteDH, PrismaticDH
+
+# link lenths in mm
+a1 = float(input("a1 = ")) # For testing, 60 mm
+a2 = float(input("a2 = ")) # For testing, 70 mm
+a3 = float(input("a3 = ")) # For testing, 30 mm
+a4 = float(input("a4 = ")) # For testing, 70 mm
+a5 = float(input("a5 = ")) # For testing, 15 mm
+a6 = float(input("a6 = ")) # For testing, 5 mm
+a7 = float(input("a7 = ")) # For testing, 10 mm
+
+# link mm to meters converter
+def mm_to_meter(a):
+    m = 1000 # 1 meter = 1000 mm
+    return a/m
+
+a1 = mm_to_meter(a1)
+a2 = mm_to_meter(a2)
+a3 = mm_to_meter(a3)
+a4 = mm_to_meter(a4)
+a5 = mm_to_meter(a5)
+a6 = mm_to_meter(a6)
+a7 = mm_to_meter(a7)
+
+# link limits converted to meters
+lm1 = float(input("lm1 = "))
+lm1 = mm_to_meter(lm1) #60mm
+
+# Create Links
+SCARA_V3 = DHRobot([
+    PrismaticDH(0,0,(0/180)*np.pi,a1,qlim=[0,lm1]),
+    RevoluteDH(0,a2,(0/180)*np.pi,0,qlim=[(-90/180)*np.pi,(90/180)*np.pi]),
+    RevoluteDH(a3,0,(0/180)*np.pi,0,qlim=[(-90/180)*np.pi,(90/180)*np.pi]),
+    PrismaticDH(0,a4,0,0,qlim=[0,0]),
+    RevoluteDH(-a5,0,(90/180)*np.pi,0,qlim=[(-90/180)*np.pi,(90/180)*np.pi]),
+    RevoluteDH(0,0,(90/180)*np.pi,0,qlim=[(-90/180)*np.pi,(90/180)*np.pi]),
+    RevoluteDH(a6+a7,0,(0/180)*np.pi,0,qlim=[(-90/180)*np.pi,(90/180)*np.pi]),
+
+], name='SCARA_V3')
+
+print(SCARA_V3)
+
+# degrees to radian converter
+def deg_to_rad(T):
+    return (T/180.0)*np.pi
+
+
+# q Paths
+q0 = np.array([0,0,0,0,0,0,0])
+q1 = np.array([mm_to_meter(float(input("d1 = "))),
+                deg_to_rad(float(input("T2 = "))),
+                deg_to_rad(float(input("T3 = "))),
+                0,
+                deg_to_rad(float(input("T4 = "))),
+                deg_to_rad(float(input("T5 = "))),
+                deg_to_rad(float(input("T6 = ")))])
+
+q2 = np.array([mm_to_meter(float(input("d1 = "))),
+                deg_to_rad(float(input("T2 = "))),
+                deg_to_rad(float(input("T3 = "))),
+                0,
+                deg_to_rad(float(input("T4 = "))),
+                deg_to_rad(float(input("T5 = "))),
+                deg_to_rad(float(input("T6 = ")))])
+
+q3 = np.array([mm_to_meter(float(input("d1 = "))),
+                deg_to_rad(float(input("T2 = "))),
+                deg_to_rad(float(input("T3 = "))),
+                0,
+                deg_to_rad(float(input("T4 = "))),
+                deg_to_rad(float(input("T5 = "))),
+                deg_to_rad(float(input("T6 = ")))])
+
+q4 = np.array([mm_to_meter(float(input("d1 = "))),
+                deg_to_rad(float(input("T2 = "))),
+                deg_to_rad(float(input("T3 = "))),
+                0,
+                deg_to_rad(float(input("T4 = "))),
+                deg_to_rad(float(input("T5 = "))),
+                deg_to_rad(float(input("T6 = ")))])
+
+q5 = np.array([mm_to_meter(float(input("d1 = "))),
+                deg_to_rad(float(input("T2 = "))),
+                deg_to_rad(float(input("T3 = "))),
+                0,
+                deg_to_rad(float(input("T4 = "))),
+                deg_to_rad(float(input("T5 = "))),
+                deg_to_rad(float(input("T6 = ")))])
+
+q6 = np.array([mm_to_meter(float(input("d1 = "))),
+                deg_to_rad(float(input("T2 = "))),
+                deg_to_rad(float(input("T3 = "))),
+                0,
+                deg_to_rad(float(input("T4 = "))),
+                deg_to_rad(float(input("T5 = "))),
+                deg_to_rad(float(input("T6 = ")))])
+
+# Trajectory commands
+traj1 = rtb.jtraj(q0,q1,50)
+traj2 = rtb.jtraj(q1,q2,50)
+traj3 = rtb.jtraj(q2,q3,50)
+traj4 = rtb.jtraj(q3,q4,50)
+traj5 = rtb.jtraj(q4,q5,50)
+traj6 = rtb.jtraj(q5,q6,50)
+
+#plot scale
+x1 = -1.0
+x2 = 1.0
+y1 = -1.0
+y2 = 1.0
+z1 = -1.0
+z2 = 1.0
+
+# Plot commands
+SCARA_V3.plot(traj1.q,limits=[x1,x2,y1,y2,z1,z2])
+SCARA_V3.plot(traj2.q,limits=[x1,x2,y1,y2,z1,z2])
+SCARA_V3.plot(traj3.q,limits=[x1,x2,y1,y2,z1,z2])
+SCARA_V3.plot(traj4.q,limits=[x1,x2,y1,y2,z1,z2])
+SCARA_V3.plot(traj5.q,limits=[x1,x2,y1,y2,z1,z2])
+SCARA_V3.plot(traj6.q,limits=[x1,x2,y1,y2,z1,z2],block=True)
+
+
+#SCARA_V3.teach(jointlabels=1)
+
+
+
+
+
+
+
